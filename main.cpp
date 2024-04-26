f908a08184b971c1e79f99158e488503c227ebb2
Shohmansur9 <shohmansurkarimov117@gmail.com>
2024-04-26 17:43:55 +0300

    in cpp language

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /// #1
+//    int n;
+//    cin >> n;
+//    if (n > 0) n++;
+//    cout << n;
+
+    /// #2
+//    int n;
+//    cin >> n;
+//    if (n > 0) n++;
+//    else n-=2;
+//    cout << n;
+
+    /// #3
+//    int n;
+//    cin >> n;
+//    if (n > 0) n++;
+//    if (n < 0) n-=2;
+//    if (n == 0) n = 10;
+//    cout << n;
+
+    /// #4
+//    int a,b,c;
+//    cin >> a >> b >> c;
+//    if (a > 0 && b > 0 && c > 0) cout << 3;
+//    if (a > 0 && b > 0 && c <= 0) cout << 2;
+//    if (a > 0 && b <= 0 && c <= 0) cout << 1;
+//    if (a <= 0 && b > 0 && c > 0) cout << 2;
+//    if (a <= 0 && b <= 0 && c > 0) cout << 1;
+//    if (b <= 0 && a > 0 && c > 0) cout << 2;
+//    if (b > 0 && a <= 0 && c <= 0) cout << 1;
+
+    /// #5
+//    int a,b,c;
+//    cin >> a >> b >> c;
+//    if (a > 0 && b > 0 && c > 0) cout << 3 << " " << 0;
+//    if (a > 0 && b > 0 && c < 0) cout << 2 << " " << 1;
+//    if (a > 0 && b < 0 && c < 0) cout << 1 << " " << 2;
+//    if (a < 0 && b > 0 && c > 0) cout << 2 << " " << 1;
+//    if (a < 0 && b < 0 && c > 0) cout << 1 << " " << 2;
+//    if (b < 0 && a > 0 && c > 0) cout << 2 << " " << 1;
+//    if (b > 0 && a < 0 && c < 0) cout << 1 << " " << 2;
+//    if (a < 0 && b < 0 && c < 0) cout << 0 << " " << 3;
+
+    /// #6
+//    int a,b;
+//    cin >> a >> b;
+//    if (a > b) cout << a;
+//    else cout << b;
+
+    /// #7
+//    int a,b;
+//    cin >> a >> b;
+//    if (a > b) cout << 2;
+//    else cout << 1;
+
+    /// #8
+//    int a,b;
+//    cin >> a >> b;
+//    if (a > b) cout << a << " " << b;
+//    else cout << b << " " << a;
+
+    /// #9
+
+
+
+
+
+
+
+
+
+}
